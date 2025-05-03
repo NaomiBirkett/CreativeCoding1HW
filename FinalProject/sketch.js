9912a39e07c4259e6c9688bfd08e1762159e5db5
NaomiBirkett <nb131357@umconnect.umt.edu>
2025-05-03 14:29:41 -0600

    Bug testing
    Trying to resolve issue with project not loading in browser.

--- FinalProject/sketch.js
+++ FinalProject/sketch.js
@@ -1,7 +1,8 @@
 let sky;
+let rhead;
+
 let rbody = [];
 let index = 0;
-let rhead;
 
 let leye;
 let reye;
@@ -15,93 +16,90 @@ let headY = 150;
 let xspeed = 0.25;
 let yspeed = 0.15;
 
-let myMusic;
-
 function preload() {
-  // Load the image files
-  sky = loadImage('Sky.jpg');
-  leye = loadImage('LeftEye.png');
-  reye = loadImage('RightEye.png');
-  rbody[0] = loadImage('HeartTorso.png');
-  rbody[1] = loadImage('RabbitTorso.png');
-  rbody[2] = loadImage('SwirlTorso.png');
-  rbody[3] = loadImage('UnicornTorso.png');
-  rhead = loadImage('Head.png');
-  rredeye = loadImage('RedRightEye.png');
-  lredeye = loadImage('RedLeftEye.png');
-  
-  // Animate door opening
-  dooropen = loadImage('DoorOpen.gif');
-  
-  // Music
-  myMusic = loadSound('IntimidatingCanvas.wav');
-}
-  
+    // Load the image files
+    sky = loadImage('Sky.jpg');
+    rhead = loadImage('Head.png');
+
+    rbody[0] = loadImage('HeartTorso.png');
+    rbody[1] = loadImage('RabbitTorso.png');
+    rbody[2] = loadImage('SwirlTorso.png');
+    rbody[3] = loadImage('UnicornTorso.png');
+
+    leye = loadImage('LeftEye.png');
+    reye = loadImage('RightEye.png');
+    rredeye = loadImage('RedRightEye.png');
+    lredeye = loadImage('RedLeftEye.png');
+    
+    // Animate door opening
+    dooropen = loadImage('DoorOpen.gif');
+  }
+
 function setup() {
-  // Set canvas
-  createCanvas(668,956);
-
- // Position images 
-  imageMode(CENTER);
+    // Set canvas
+    createCanvas(668,956);
   
-  // Pause gif by default
-  dooropen.pause();
+   // Position images 
+    imageMode(CENTER);
+    
+    // Pause gif by default
+    dooropen.pause();
+  
+    // Resize the images
+    rhead.resize(184, 148);
 
-  // Resize the images
-  leye.resize(37, 35);
-  reye.resize(36, 35);
-  lredeye.resize(45, 44);
-  rredeye.resize(44, 48);
-  rbody[0].resize(668, 956);
-  rbody[1].resize(668, 956);
-  rbody[2].resize(668, 956);
-  rbody[3].resize(668, 956);
-  rhead.resize(184, 148);
-  dooropen.resize(112,110);
-}
+    rbody[0].resize(668, 956);
+    rbody[1].resize(668, 956);
+    rbody[2].resize(668, 956);
+    rbody[3].resize(668, 956);
+
+    leye.resize(37, 35);
+    reye.resize(36, 35);
+    lredeye.resize(45, 44);
+    rredeye.resize(44, 48);
+    
+    dooropen.resize(112,110);
+  }
 
 function draw() {
-  background(52, 175, 209);
-  image(sky,334,478);
-
-  // Play music
-  mySound.play();
-
-  // Draw the robot
-  for(i = 0; i < rbody.length; i++){
-  image(rbody[index],320,500);
-  }
+    background(52, 175, 209);
+    image(sky,334,478);
   
-  image(rhead,headX,headY);
-  image(leye,256,140);
-  image(leye,345,145);
+    // Draw the robot
+    for(i = 0; i < rbody.length; i++){
+    image(rbody[index],320,500);
+    }
+    
+    image(rhead,headX,headY);
+    image(leye,256,140);
+    image(leye,345,145);
+    
+    image(dooropen,305,324);
   
-  image(dooropen,305,324);
-
-// animate head bob
-headX = headX + xspeed;
-if(headX >= 312 || headX <= 307) {
-      xspeed *= -1;
-  }
-headY = headY + yspeed;
-if(headY >= 151 || headY <= 148) {
-      yspeed *= -1;
-  }
- 
-// open the door and reveal images  
-if (mouseIsPressed) {
-  index = index + 1;
+  // animate head bob
+  headX = headX + xspeed;
+  if(headX >= 312 || headX <= 307) {
+        xspeed *= -1;
+    }
+  headY = headY + yspeed;
+  if(headY >= 151 || headY <= 148) {
+        yspeed *= -1;
+    }
+   
+  // open the door and reveal images  
+  if (mouseIsPressed) {
+    index = index + 1;
+    
+    if (index == 4) {
+      index = 0;
+    }
+    
+    image(lredeye, 254,140);
+    image(rredeye, 343,146);
+    image(dooropen,305,324);
+    dooropen.setFrame(0);
+    dooropen.play();
+    }
   
-  if (index == 4) {
-    index = 0;
   }
-  
-  image(lredeye, 254,140);
-  image(rredeye, 343,146);
-  image(dooropen,305,324);
-  dooropen.setFrame(0);
-  dooropen.play();
-  }
-
-}
-
+    
